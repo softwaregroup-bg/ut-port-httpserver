3bfc30c2b8945f4dd08fd55b3adb01acbc75b7ae
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2015-04-03 17:52:41 +0300

    add error when no return value is specified for a promise

--- index.js
+++ index.js
@@ -69,6 +69,7 @@
                 request.payload.params.$$ = {authentication: request.payload.authentication};
                 when(when.lift(method)(request.payload.params))
                     .then(function (r) {
+                        if (!r) throw new Error('Add return value of method ' + request.payload.method + ' for not returned promise');
                         if (r.$$) {
                             delete r.$$;
                         }
