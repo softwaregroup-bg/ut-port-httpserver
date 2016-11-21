5acc38777a79afa52a6512fe7e9bce8627a9942a
elin.angelov <elin.angelov@softwaregroup-bg.com>
2016-11-21 09:56:48 +0200

    fix: removed logic that is not in use anymore;few errors are red from errors.js

--- errors.js
+++ errors.js
@@ -1,11 +1,15 @@
 var create = require('ut-error').define;
 
 var HttpServer = create('HttpServer');
+var MethodNotFound = create('MethodNotFound', 'HttpServer');
+var NotPermitted = create('NotPermitted', 'HttpServer');
 var ValidationNotFound = create('ValidationNotFound', 'HttpServer');
 var InvalidRequest = create('InvalidRequest', 'HttpServer');
 module.exports = {
     HttpServer: HttpServer,
     ValidationNotFound: ValidationNotFound,
+    NotPermitted: NotPermitted,
+    MethodNotFound: MethodNotFound,
     InvalidRequest: InvalidRequest
 };
 
