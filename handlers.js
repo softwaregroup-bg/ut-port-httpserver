c0b3cdddac370a1ee5ec7203dac1f34ac3cc14ae
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-02-10 11:18:13 +0200

    fix: x-forward

--- handlers.js
+++ handlers.js
@@ -263,11 +263,20 @@ module.exports = function(port) {
 
         var identityCheckParams;
         if (request.payload.method === 'identity.check') {
-            identityCheckParams = assign({}, request.payload.params, {ip: (port.config.allowXFF && request.headers['x-forwarded-for'])
-            ? request.headers['x-forwarded-for'] : request.info.remoteAddress}, request.auth.credentials);
+            identityCheckParams = assign({}, request.payload.params);
         } else {
-            identityCheckParams = assign({actionId: request.payload.method, ip: request.info.remoteAddress}, request.auth.credentials);
+            identityCheckParams = {actionId: request.payload.method};
         }
+        identityCheckParams = assign(
+            {},
+            identityCheckParams,
+            request.auth.credentials,
+            {ip: (
+                (port.config.allowXFF && request.headers['x-forwarded-for'])
+                ? request.headers['x-forwarded-for']
+                : request.info.remoteAddress
+            )}
+        );
 
         port.bus.importMethod('identity.check')(identityCheckParams)
         .then((res) => {
