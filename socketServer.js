11875181b8286d59d3cb59c50614e02ba084c2c0
elin.angelov <elin.angelov@softwaregroup-bg.com>
2017-05-18 15:16:09 +0300

    fix: client verify on right place

--- socketServer.js
+++ socketServer.js
@@ -15,41 +15,64 @@ var util = {
 
 function SocketServer() {
     this.router = new Router();
+    this.handlers = {};
     this.rooms = {};
     this.wss = null;
+    this.routeId = 0;
 }
 
 SocketServer.prototype.start = function start(server) {
     this.wss = new ws.Server({
-        server: server
+        server: server,
+        verifyClient: (i, cb) => {
+            var context = this.router.route(i.req.method.toLowerCase(), i.req.url);
+            if (context.isBoom) {
+                cb(false, context.output.payload.statusCode, context.output.payload.error);
+            } else {
+                this.handlers[context.route].verifyClient(i, cb);
+            }
+        }
     });
     this.wss.on('connection', (socket) => {
         var context = this.router.route(socket.upgradeReq.method.toLowerCase(), socket.upgradeReq.url);
-        context.isBoom ? socket.terminate() : context.route(this.router.analyze(socket.upgradeReq.url).fingerprint, socket);
+        this.handlers[context.route].handler(this.router.analyze(socket.upgradeReq.url).fingerprint, socket);
     });
 };
 
-SocketServer.prototype.registerPath = function registerPath(path, beforeHandlerHook, opts) {
+SocketServer.prototype.registerPath = function registerPath(path, verifyClient, opts) {
+    let id = ++this.routeId;
     this.router.add({
+        id: id,
         method: 'get',
         path: path
-    }, (roomId, socket) => {
-        let afterHandlerHook = ((socket, roomId) => (err, resp) => {
-            if (err) {
-                return socket.close();
-            }
-            if (!this.rooms[roomId]) {
-                this.rooms[roomId] = [];
-            }
-            var i = this.rooms[roomId].push(socket) - 1;
-            socket.on('close', () => (this.rooms[roomId].splice(i, 1)));
-        })(socket, roomId);
-        if (beforeHandlerHook && typeof (beforeHandlerHook) === 'function') {
-            beforeHandlerHook(socket, roomId, afterHandlerHook);
-        } else {
-            afterHandlerHook();
+    }, id);
+    if (!this.handlers[id]) {
+        this.handlers[id] = {};
+    }
+    this.handlers[id].handler = (roomId, socket) => {
+        if (!this.rooms[roomId]) {
+            this.rooms[roomId] = [];
         }
-    });
+        var i = this.rooms[roomId].push(socket) - 1;
+        socket.on('close', () => (this.rooms[roomId].splice(i, 1)));
+    };
+    this.handlers[id].verifyClient = (info, cb) => {
+        if (verifyClient && typeof (verifyClient) === 'function') {
+            verifyClient(info.req, this.router.analyze(info.req.url).fingerprint, (err) => {
+                if (err) {
+                    if (err.isBoom) {
+                        cb(false, err.output.payload.statusCode, err.output.payload.error);
+                    } else {
+                        cb(false, 500, 'Internal Server Error');
+                    }
+                } else {
+                    cb(true);
+                }
+            });
+        } else {
+            cb(true);
+        }
+    };
 };
 
 SocketServer.prototype.publish = function publish(data, message) {
