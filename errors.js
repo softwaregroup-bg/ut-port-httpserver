3846ba0376c4c9aad9c961852dd9019598ee02c1
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-11-10 09:12:37 +0200

    fix: error namespace

--- errors.js
+++ errors.js
@@ -3,8 +3,8 @@ const create = require('ut-error').define;
 const HttpServer = create('httpServerPort');
 module.exports = {
     HttpServer: HttpServer,
-    ValidationNotFound: create('validationNotFound', 'HttpServer'),
-    NotPermitted: create('notPermitted', 'HttpServer'),
-    MethodNotFound: create('methodNotFound', 'HttpServer'),
-    InvalidRequest: create('invalidRequest', 'HttpServer')
+    ValidationNotFound: create('validationNotFound', HttpServer),
+    NotPermitted: create('notPermitted', HttpServer),
+    MethodNotFound: create('methodNotFound', HttpServer),
+    InvalidRequest: create('invalidRequest', HttpServer)
 };
