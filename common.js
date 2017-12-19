fde58503e5e69ca823156a6e976d18c5cf56512c
stambata <stamen.peev@softwaregroup-bg.com>
2017-12-19 05:27:38 +0200

    fix: meta validations

--- common.js
+++ common.js
@@ -1,14 +1,40 @@
 'use strict';
 const os = require('os');
+const joi = require('joi');
 const osName = [os.type(), os.platform(), os.release()].join(':');
 
-module.exports = {
-    initMetadataFromRequest
-};
+const metaSchema = joi.object().keys({
+    timeout: joi.number().positive(),
+    auth: joi.object().keys({
+        actorId: joi.number().positive().integer(),
+        exp: joi.number().positive().integer(),
+        iat: joi.number().positive().integer(),
+        scopes: joi.array(),
+        sessionId: joi.string().max(36),
+        timezone: joi.string(),
+        xsrfToken: joi.string().max(36)
+    }).allow(null),
+    method: joi.string().max(100),
+    opcode: joi.string(),
+    mtid: joi.string(),
+    requestHeaders: joi.object(),
+    ipAddress: joi.string().max(50),
+    frontEnd: joi.string().max(250),
+    latitude: joi.number(),
+    longitude: joi.number(),
+    localAddress: joi.string().max(50),
+    hostName: joi.string().max(50),
+    localPort: joi.number(),
+    machineName: joi.string().max(50),
+    os: joi.string().max(50),
+    version: joi.string(),
+    serviceName: joi.string().max(50),
+    deviceId: joi.string().max(50)
+});
 
 function initMetadataFromRequest(request = {}, port = {}) {
     let bus = port.bus || {};
-    return {
+    const {error, value} = metaSchema.validate({
         timeout: port.timing && request.payload.timeout && port.timing.after(request.payload.timeout),
         auth: request.auth.credentials,
         method: request.payload && request.payload.method,
@@ -27,5 +53,14 @@ function initMetadataFromRequest(request = {}, port = {}) {
         version: bus.config && bus.config.version,
         serviceName: bus.config && (bus.config.implementation + (bus.config.service ? '/' + bus.config.service : '')),
         deviceId: request.headers && request.headers.deviceId
-    };
+    }, {abortEarly: false});
+
+    if (error) {
+        throw error;
+    }
+    return value;
 }
+
+module.exports = {
+    initMetadataFromRequest
+};
